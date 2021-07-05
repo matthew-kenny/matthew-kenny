5937d48d6e92c7150875f568ee139fcec3ed1477
Matthew Kenny <74065636+matthew-kenny@users.noreply.github.com>
2021-07-05 21:17:05 +0100

    improve deb compatibility

--- microk8s.sh
+++ microk8s.sh
@@ -14,6 +14,7 @@ sudo echo '$MACHINENAME1 $IPADDRESS1' >> /etc/hosts
 sudo echo '$MACHINENAME2 $IPADDRESS2' >> /etc/hosts
 sudo echo '$MACHINENAME3 $IPADDRESS3' >> /etc/hosts
 sudo apt update | sudo apt upgrade –y 
+sudo touch $HOME/.hushlogin
 sudo apt install neofetch snapd
 sudo echo 'neofetch' >> .bashrc
 sudo snap install microk8s --classic --channel=latest/edge/ha-preview 
