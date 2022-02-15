b45319193b3c7b6d298db34e23069b4cdd7d8ea3
Matthew Kenny <74065636+matthew-kenny@users.noreply.github.com>
2022-02-15 00:08:02 +0000

    Update docker module

--- WindowsServer.ps1
+++ WindowsServer.ps1
@@ -1,7 +1,11 @@
 oSet-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
-choco install Containers Microsoft-Hyper-V --source windowsfeatures
-choco install nano wget powershell-core winfetch adguardhome nssm anydesk.install docker-engine microsoft-windows-terminal k3d
+choco install Containers --source windowsfeatures
+choco install nano wget powershell-core winfetch adguardhome nssm anydesk.install microsoft-windows-terminal
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider
+Restart-Computer -Force
+docker version
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\PowerShell\7\pwsh.exe" -PropertyType String -Force
 mkdir %username%\Documents\PowerShell
 nano %username%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
@@ -13,6 +17,6 @@ cp %username%\.ssh\id_ed25519.pub C:\ProgramData\ssh\administrators_authorized_k
 icacls.exe "C:\ProgramData\ssh\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
 New-NetFirewallRule -DisplayName AdGuard -Profile @('Domain', 'Private', 'Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('53', '80', '443', '3000')
 New-NetFirewallRule -DisplayName AdGuard2 -Profile @('Domain', 'Private', 'Public') -Direction Inbound -Action Allow -Protocol UDP -LocalPort @('53')
-New-NetFirewallRule -DisplayName "Docker" -Direction Inbound -Program "C:\Program Files\Docker\Docker\resources\com.docker.backend.exe" -Action Allow
-nssm.exe install adguardhome
-Set-Service docker -startuptype automatic
+// New-NetFirewallRule -DisplayName "Docker" -Direction Inbound -Program "C:\Program Files\Docker\Docker\resources\com.docker.backend.exe" -Action Allow
+// nssm.exe install adguardhome
+// Set-Service docker -startuptype automatic
