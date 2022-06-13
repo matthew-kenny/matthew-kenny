4671ad249bd9a0342d24c52255e93bf76747e42e
Matthew Kenny <W18012455@northumbria.ac.uk>
2022-06-13 23:51:25 +0100

    add Mac

---
+++ mac.sh
@@ -0,0 +1,14 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Core apps
+Tinkertool
+Magnet
+Reeder
+brew install --cask cheatsheet Dropzone microsoft-edge the-unarchiver powershell microsoft-office microsoft-teams termius neofetch spotify 
+
+
+Overcast.fm #Apple Silicon
+
+# Install Programming apps
+brew install --cask sourcetree iterm2 visual-studio-code visual-studio intellij-idea awscli azure-cli terraform git docker docker-compose
