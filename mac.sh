e2cfd1ce7868a1755905e01e407b26faeb15f47e
Matthew Kenny <74065636+matthew-kenny@users.noreply.github.com>
2022-06-21 12:59:58 +0100

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,14 +1,13 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Core apps
-Tinkertool
+# App Store Paid Apps
 Magnet
 Reeder
+
+# Install Core apps
 brew install --cask cheatsheet Dropzone microsoft-edge the-unarchiver powershell microsoft-office microsoft-teams termius neofetch spotify microsoft-remote-desktop slack
 
-
-Overcast.fm #Apple Silicon
-
 # Install Programming apps
-brew install --cask sourcetree iterm2 visual-studio-code visual-studio intellij-idea awscli azure-cli terraform git docker docker-compose
+brew install --cask sourcetree iterm2 visual-studio-code visual-studio intellij-idea docker
+brew install awscli azure-cli terraform git docker-compose
