2bc4b908c80112eb8950ea4e9382ae7122260425
Matthew Kenny <74065636+matthew-kenny@users.noreply.github.com>
2022-07-05 15:52:13 +0100

    Separate Core and Containers

--- WindowsServer.ps1
+++ WindowsServer.ps1
@@ -7,21 +7,26 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 ## Disable confirmation for installing apps ##
 choco feature enable -n allowGlobalConfirmation
 
-## Install Container System and enable OpenSSH ##
-choco install Containers Microsoft-Hyper-V --source windowsfeatures
-choco install nano wget powershell-core winfetch adguardhome nssm anydesk.install microsoft-windows-terminal
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Install-Package -Name docker -ProviderName DockerMsftProvider
-Restart-Computer -Force
-docker version
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\PowerShell\7\pwsh.exe" -PropertyType String -Force
+## Install Core Apps ##
+choco install powershell-core winfetch anydesk.install microsoft-windows-terminal
 mkdir %username%\Documents\PowerShell
 nano %username%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 winfetch
+
+## Install OpenSSH ##
 ssh-keygen -t ed25519
-cat %username%\.ssh\id_ed25519
-cat %username%\.ssh\id_ed25519.pub
-cp %username%\.ssh\id_ed25519.pub C:\ProgramData\ssh\administrators_authorized_keys
+cat .ssh\id_ed25519
+cat .ssh\id_ed25519.pub
+cp .ssh\id_ed25519.pub C:\ProgramData\ssh\administrators_authorized_keys
 icacls.exe "C:\ProgramData\ssh\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\PowerShell\7\pwsh.exe" -PropertyType String -Force
+
+
+## Install Containers ##
+choco install Containers Microsoft-Hyper-V --source windowsfeatures
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider
 New-NetFirewallRule -DisplayName "Docker" -Direction Inbound -Program "C:\Program Files\Docker\Docker\resources\com.docker.backend.exe" -Action Allow
+docker version
+Restart-Computer -Force
